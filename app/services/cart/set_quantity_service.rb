9a0a4c86c4b0c874d853591980e78d7da133af71
Thea Choem <29684683+theachoem@users.noreply.github.com>
2024-12-04 12:34:10 +0700

    Add set quantity service

--- app/services/cart/set_quantity_service.rb
+++ app/services/cart/set_quantity_service.rb
@@ -1,9 +1,44 @@
 module Cart
   class SetQuantityService
-    def initialize
+    attr_reader :order_id, :variant_id, :quantity, :error
+
+    def success? = @error == nil
+    def initialize(order_id:, options: {})
+      @order_id = order_id
+      @variant_id = options[:variant_id]
+      @quantity = [ options[:quantity]&.to_i, 1 ].max
+      @error = nil
     end
 
     def call
+      ActiveRecord::Base.transaction do
+        begin
+          set_quantity
+        rescue StandardError => e
+          @error = e.message
+          Rails.logger.error("Error during transaction: #{e.message}")
+          raise ActiveRecord::Rollback
+        end
+      end
+      success?
+    end
+
+    private
+
+    def set_quantity
+      # 1 db operation
+      variant = Variant.find(variant_id)
+      raise QuantityNotFulfill, "Item only available #{variant.total_inventory_units}" unless variant.can_fulfill?(quantity)
+
+      # 1 db operation
+      order = Order.find(order_id)
+      raise CartDoesNotAllowedToAddItem, "Current cart #{order.state}" if order.allowed_modify_item?
+
+      # 1 db operation
+      order.line_items.where(variant_id: variant_id).update_all(quantity: quantity)
+
+      # 6 db operation
+      RecalculateService.new(order_id: order_id).call
     end
   end
 end
--- spec/services/cart/set_quantity_service_spec.rb
+++ spec/services/cart/set_quantity_service_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Cart::AddItemService do
+RSpec.describe Cart::SetQuantityService do
   let(:stock_item) { create(:stock_item, inventory_units: 4) }
   let(:variant) { create(:variant, stock_items: [ stock_item ]) }
 
   let!(:order) { create(:order) }
 
+  before do
+    Cart::AddItemService.new(order_id: order.id, options: { variant_id: variant.id, quantity: 1 }).call
+  end
+
   describe '#call' do
-    it 'perform 21 db query' do
+    it 'perform 9 db query' do
       subject = described_class.new(order_id: order.id, options: { variant_id: variant.id, quantity: 2 })
       query_count = count_queries { subject.call }
 
-      expect(query_count).to eq 21
+      expect(query_count).to eq 9
+      expect(order.line_items.last.quantity).to eq 2
     end
 
     context 'when variant inventory units is more than needed' do
       let(:stock_item) { create(:stock_item, inventory_units: 3, backorderable: false) }
       let(:variant) { create(:variant, stock_items: [ stock_item ]) }
 
-      it 'add item to cart' do
+      it 'set quantity to item' do
         subject = described_class.new(order_id: order.id, options: { variant_id: variant.id, quantity: 2 })
         subject.call
 
@@ -32,11 +37,12 @@ RSpec.describe Cart::AddItemService do
       let(:stock_item) { create(:stock_item, inventory_units: 2, backorderable: false) }
       let(:variant) { create(:variant, stock_items: [ stock_item ]) }
 
-      it 'does not add item to cart' do
+      it 'does not set quantity to item' do
         subject = described_class.new(order_id: order.id, options: { variant_id: variant.id, quantity: 3 })
         subject.call
 
-        expect(order.line_items.size).to eq 0
+        expect(order.line_items.size).to eq 1
+        expect(order.line_items.last.quantity).to eq 1
         expect(subject.success?).to be false
         expect(subject.error).to eq "Item only available 2"
       end
